e737f46b8a7d7cb3b0decb879a19a32de470e36e
lemmonchess333 <neckarines12@gmail.com>
2026-02-19 12:27:41 -0300

    Update variationBank.ts

--- src/features/program/variationBank.ts
+++ src/features/program/variationBank.ts
@@ -1,1 +1,20 @@
-hi
+export const variationBank = {
+  squat: [
+    "Low Bar Squat",
+    "Paused Squat",
+    "Tempo Squat",
+    "Heel Elevated Zercher"
+  ],
+  hinge: [
+    "Conventional Deadlift",
+    "Paused Deadlift",
+    "Deficit Deadlift",
+    "Romanian Deadlift"
+  ],
+  horizontal_push: [
+    "Competition Bench",
+    "Paused Bench",
+    "Larsen Press",
+    "Close Grip Bench"
+  ],
+};
