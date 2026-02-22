b1f90e38e0c1fe3d7d22b3cdb57582ec0130144f
lemmonchess333 <neckarines12@gmail.com>
2026-02-21 22:47:39 -0300

    Update History.tsx

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useMemo } from "react";
 import { useHistoryData } from "@/hooks/useFirestore";
 import { useMeals } from "@/hooks/useMeals";
 import { useWorkouts } from "@/hooks/useWorkouts";
