eb1af1c8bd8df3ad2759ef1a130bcd7f388abc22
lemmonchess333 <neckarines12@gmail.com>
2026-03-05 00:23:24 -0300

    Update RunSummary.tsx

--- src/pages/RunSummary.tsx
+++ src/pages/RunSummary.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { addDoc, collection, Timestamp } from 'firebase/firestore';
 import { db } from '../lib/firebase';
